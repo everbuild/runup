c7a8ed1a41d62d9e792a8893609c78021c48fa51
Evert <evert.bauwens@gmail.com>
2017-06-13 17:56:37 +0200

    cli + usage

--- cli.js
+++ cli.js
@@ -1,21 +1,29 @@
 #!/usr/bin/env node
 const start = new Date().valueOf()
 const minimist = require('minimist')
-const runup = require('./lib/index')
 
 const argv = minimist(process.argv.slice(2), {
     default: {
-        model: '?project.js'
+        model: '?./project'
     },
     alias: {
-        model: 'm'
+        model: 'm',
+        help: 'h'
     }
 })
 
-const mode = argv._[0] || 'dev'
-const modelFiles = argv.model.split(',').map(f => f.trim())
+if(argv.help) {
+    console.log(require('fs').readFileSync(require.resolve('./help.txt'), 'utf-8'))
+} else {
+    const runup = require('./lib/index')
+    const mode = argv._[0] || 'dev'
+    const modelFiles = argv.model.split(',').map(f => f.trim())
 
-runup(mode, modelFiles).then(
-    () => console.log(`${mode === 'build' ? 'total' : 'startup'} time: ${new Date().valueOf() - start}ms`),
-    console.error
-)
+    runup(mode, modelFiles).then(
+        () => console.log(`${mode === 'build' ? 'total' : 'startup'} time: ${new Date().valueOf() - start}ms`),
+        error => {
+            console.error(error)
+            process.exit(1)
+        }
+    )
+}
