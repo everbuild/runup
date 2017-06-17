706e84bd9b4c5f2e6a82582932101c190f67089d
Evert <evert.bauwens@gmail.com>
2017-06-17 12:38:15 +0200

    migrated util into engine better model handling: runup API now accepts a model in stead of paths managed verbose output a bit better

--- cli.js
+++ cli.js
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
-const start = new Date().valueOf()
 const minimist = require('minimist')
 
 const argv = minimist(process.argv.slice(2), {
     default: {
-        model: '?./project'
+        model: '?./project',
+        verbose: false
     },
     alias: {
         model: 'm',
+        verbose: 'v',
         help: 'h'
     }
 })
@@ -16,14 +17,15 @@ if(argv.help) {
     console.log(require('fs').readFileSync(require.resolve('./help.txt'), 'utf-8'))
 } else {
     const runup = require('./lib/index')
-    const mode = argv._[0] || 'dev'
-    const modelFiles = argv.model.split(',').map(f => f.trim())
 
-    runup(mode, modelFiles).then(
-        () => console.log(`${mode === 'build' ? 'total' : 'startup'} time: ${new Date().valueOf() - start}ms`),
-        error => {
-            console.error(error)
-            process.exit(1)
-        }
-    )
+    const model = {
+        mode: argv._[0] || 'dev',
+        extend: argv.model.split(',').map(f => f.trim()),
+        verbose: argv.verbose
+    }
+
+    runup(model).catch(error => {
+        console.error(error)
+        process.exit(1)
+    })
 }
